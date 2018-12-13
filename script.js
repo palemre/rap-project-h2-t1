54ac436a1e18547760d2dce93bd5754d0e88dace
palemre <palandoken.e@gmail.com>
2018-12-13 22:54:30 +0100

    :sparkles: History of Rap intro done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*******************
  * I) DISC LOADER : DELETE AFTER ANIMATION FINISHED
  * II) HEADPHONE WARNING : DISPLAY AFTER DISC & DELETE AFTER ANIMATION FINISHED
- * III) RAP HISTORY : DISPLAY WHEN NEEDED
+ * III) RAP HISTORY : DISPLAY MAIN RAP WRAPPER
  ******************/
 
 /*******************
@@ -15,11 +15,16 @@ const discDisplay = setInterval(() =>
 }, 5000)
 
 /*******************
-******************** HEADPHONE WARNING
+******************** HEADPHONE WARNING + MAIN RAP WRAPPER DISPLAY
 *******************/
 const headphone = document.querySelector('.headphone')
 const headphoneWarning = document.querySelector('.headphone-warning-text')
 const startButton = document.querySelector('.start-button')
+
+const wrapperRapHistory = document.querySelector('.wrapper-rap-history')
+const verticalDotsNav = document.querySelector('.vertical-dots-nav')
+const hamburgerMenuLines = document.querySelector('.hamburger-menu-lines')
+
 const headphoneDisplay = setInterval(() =>
 {
     headphone.style.display = `inline-block`
@@ -35,14 +40,24 @@ startButton.addEventListener('click', () =>
     startButton.classList.add('headphone-warning-animation')
     const deleteHeadphoneWarning = setInterval(() =>
     {
+        //DELETE HEADPHONE WARNING
         headphone.remove()
         headphoneWarning.remove()
         startButton.remove()
+
+        //RAP DISPLAY
+        wrapperRapHistory.style.display = `inline`
+        verticalDotsNav.style.display = `inline`
+        hamburgerMenuLines.style.display = `inline`
+
+        wrapperRapHistory.classList.add('display-wrapper-rap-history')
+        verticalDotsNav.classList.add('display-wrapper-rap-history')
+        hamburgerMenuLines.classList.add('display-wrapper-rap-history')
+
+        const rapTypes = document.querySelector('.rap-types')
+        const offsetHeight = document.body.offsetHeight;
+        rapTypes.style.marginTop =`${offsetHeight}px`;
+
         window.clearInterval(deleteHeadphoneWarning)
     }, 1000)
-
 })
-
-/*******************
-******************** START HISTORY OF RAP
-*******************/
