0b9261c7b3a2841f554f77bfaa76a66ccc4550e5
palemre <palandoken.e@gmail.com>
2018-12-14 16:04:05 +0100

    :sparkles: Rap intro & Rap origin done. Next -> Finish rap's types

--- script.js
+++ script.js
@@ -26,7 +26,10 @@ const startButton = document.querySelector('.start-button')
 const wrapperRapHistory = document.querySelector('.wrapper-rap-history')
 const verticalDotsNav = document.querySelector('.vertical-dots-nav')
 const hamburgerMenuLines = document.querySelector('.hamburger-menu-lines')
-const rapTypes = document.querySelector('.rap-types')
+const rapOrigin = wrapperRapHistory.querySelector('.rap-origin')
+const rapTypes = wrapperRapHistory.querySelector('.rap-types')
+const rapMosaicBackgroundImage = wrapperRapHistory.querySelector('.rap-history-background')
+
 const offsetHeight = document.body.offsetHeight
 
 const headphoneDisplay = setInterval(() =>
@@ -57,12 +60,46 @@ startButton.addEventListener('click', () =>
         verticalDotsNav.classList.add('display-wrapper-rap-history')
         hamburgerMenuLines.classList.add('display-wrapper-rap-history')
 
-        //RAP STYLE DISPLAY
-        rapTypes.style.marginTop =`${offsetHeight}px`
+        //RAP MOSAIC BACKGROUND
+        rapMosaicBackgroundImage.style.height = `${offsetHeight}px`
+
+        //RAP ORIGIN DISPLAY
+        rapOrigin.style.marginTop =`${offsetHeight}px`
+        rapOrigin.style.height =`${offsetHeight}px`
+
+        //RAP TYPES DISPLAY
+        // rapTypes.style.marginTop =`${offsetHeight}px`
         rapTypes.style.height =`${offsetHeight}px`
 
         window.clearInterval(deleteHeadphoneWarning)
     }, 1000)
+})
+
+
+//RAP'S ORIGIN TIMELINE PULSE BUTTONS
+const pulseButtons = rapOrigin.querySelectorAll('.pulse-button')
+const pulseButtonText = rapOrigin.querySelector('.pulse-button-text')
+let selectedButton = 0
+const rapOriginTexts = ['Le genre se développe en tant que mouvement culturel et artistique aux États-Unis, à New York, dans le South Bronx au début des années 1970.', 'Les premiers raps sont réalisés par des maîtres de cérémonies faisant des rimes simples pour mettre de l’ambiance en soirée. Ceux-ci sont italiens contrairement à ce que l’on pourrait croire.', 'Sortie de « Rapper’s Delight » du groupe Sugarhill Gag, qui devient le premier tube rap dans le monde.', 'Début de la popularité, explosion du rap dans le monde entier. C’est à cette période que le rap vas arriver en europe notamment en France et en Angleterre.', 'Sortie de “the message” de Grandmaster Flash qui engendre une révolution par sa nouvelle approche au rap']
+
+//CHANGE TEXT ACCORDING TO CLICKED BUTTON ON TIMELINE
+rapOrigin.addEventListener('click', () =>
+{
+    for (let i = 0; i < pulseButtons.length; i++)
+    {
+        pulseButtons[i].addEventListener('click', () =>
+        {
+            selectedButton = i
+            pulseButtons[i].style.backgroundColor = '#FFBA00'
+            pulseButtons[i].classList.add('pulse-button-animation')
+            pulseButtonText.innerHTML = rapOriginTexts[i]
+        })
+        if (i != selectedButton)
+        {
+            pulseButtons[i].style.backgroundColor = `#FFF`
+            pulseButtons[i].classList.remove('pulse-button-animation')
+        }
+    }
 })
 
 /*******************
@@ -79,22 +116,67 @@ let percentOfScroll
 const firstVerticalDot = verticalDotsNav.querySelector('.first-dot')
 const secondVerticalDot = verticalDotsNav.querySelector('.second-dot')
 const thirdVerticalDot = verticalDotsNav.querySelector('.third-dot')
+const fourthVerticalDot = verticalDotsNav.querySelector('.fourth-dot')
+const fifthVerticalDot = verticalDotsNav.querySelector('.fifth-dot')
+const sixthVerticalDot = verticalDotsNav.querySelector('.sixth-dot')
 
 window.addEventListener('scroll', () =>
 {
     percentOfScroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100
 
-    if (percentOfScroll >= 0 && percentOfScroll < 35)
+    if (percentOfScroll >= 0 && percentOfScroll < 15)
     {
         firstVerticalDot.style.background = `#FFF`
         secondVerticalDot.style.background = `transparent`
         thirdVerticalDot.style.background = `transparent`
+        fourthVerticalDot.style.background = `transparent`
+        fifthVerticalDot.style.background = `transparent`
+        sixthVerticalDot.style.background = `transparent`
     }
-    else if (percentOfScroll >= 35)
+    else if (percentOfScroll >= 15 && percentOfScroll < 30)
     {
         firstVerticalDot.style.background = `transparent`
         secondVerticalDot.style.background = `#FFF`
         thirdVerticalDot.style.background = `transparent`
+        fourthVerticalDot.style.background = `transparent`
+        fifthVerticalDot.style.background = `transparent`
+        sixthVerticalDot.style.background = `transparent`
+    }
+    else if (percentOfScroll >= 30 && percentOfScroll < 45)
+    {
+        firstVerticalDot.style.background = `transparent`
+        secondVerticalDot.style.background = `transparent`
+        thirdVerticalDot.style.background = `#FFF`
+        fourthVerticalDot.style.background = `transparent`
+        fifthVerticalDot.style.background = `transparent`
+        sixthVerticalDot.style.background = `transparent`
+    }
+    else if (percentOfScroll >= 45 && percentOfScroll < 60)
+    {
+        firstVerticalDot.style.background = `transparent`
+        secondVerticalDot.style.background = `transparent`
+        thirdVerticalDot.style.background = `transparent`
+        fourthVerticalDot.style.background = `#FFF`
+        fifthVerticalDot.style.background = `transparent`
+        sixthVerticalDot.style.background = `transparent`
+    }
+    else if (percentOfScroll >= 60 && percentOfScroll < 75)
+    {
+        firstVerticalDot.style.background = `transparent`
+        secondVerticalDot.style.background = `transparent`
+        thirdVerticalDot.style.background = `transparent`
+        fourthVerticalDot.style.background = `transparent`
+        fifthVerticalDot.style.background = `#FFF`
+        sixthVerticalDot.style.background = `transparent`
+    }
+    else if (percentOfScroll >= 75)
+    {
+        firstVerticalDot.style.background = `transparent`
+        secondVerticalDot.style.background = `transparent`
+        thirdVerticalDot.style.background = `transparent`
+        fourthVerticalDot.style.background = `transparent`
+        fifthVerticalDot.style.background = `transparent`
+        sixthVerticalDot.style.background = `#FFF`
     }
 })
 
